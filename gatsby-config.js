dd724c37e13151efe71a73e79ff54628c5d112dd
joshnorlin <jnorlin@outlook.com>
2025-05-28 14:27:24 -0600

    Update gatsby-config.js
    Added pertinent information

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,17 +3,17 @@ module.exports = {
     // Site URL for when it goes live
     siteUrl: `https://monumental-chimera-b1ace3.netlify.app/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Josh Norlin',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Josh Norlin | Full-Stack Developer`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `Computer Science student, building software.`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    /*author: `@rfitzio`,*/
     // Optional: Github account URL
-    github: `https://github.com/RyanFitzgerald`,
+    github: `https://github.com/joshnorlin`,
     // Optional: LinkedIn account URL
-    linkedin: `https://github.com/RyanFitzgerald/devfolio`,
+    linkedin: `https://www.linkedin.com/in/joshuanorlin/`,
     // Content of the About Me section
     about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
@@ -31,28 +31,18 @@ module.exports = {
         link: 'https://extensionkit.io/?ref=devfolio',
       },
       {
-        name: 'Another Cool Project',
+        name: 'SmartWeather',
         description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
+          'Cross-platform, TypeScript-powered mobile app built with React Native and Expo, delivering real-time, location-aware weather insights with a sleek, responsive UI.',
         link: 'https://github.com/RyanFitzgerald/devfolio',
       },
     ],
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
-        name: 'Acme Corp',
-        description: 'Full-Stack Developer, February 2020 - Present',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-      {
-        name: 'Globex Corp',
-        description: 'Full-Stack Developer, December 2017 - February 2020',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-      {
-        name: 'Hooli',
-        description: 'Full-Stack Developer, May 2015 - December 2017',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Red Aspen',
+        description: 'Database Engineer, May 2023 - August 2024',
+        link: 'https://www.linkedin.com/in/joshuanorlin/',
       },
     ],
     // Optional: List your skills, they must have `name` and `description`.
@@ -60,11 +50,11 @@ module.exports = {
       {
         name: 'Languages & Frameworks',
         description:
-          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
+          'JavaScript (ES6+), Node.js, React, React Native, PHP, C',
       },
       {
         name: 'Databases',
-        description: 'MongoDB, PostreSQL, MySQL',
+        description: 'PostreSQL, SQL Server, PL/SQL',
       },
       {
         name: 'Other',
